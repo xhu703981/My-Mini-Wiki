1aa9b1fa467472eacd17bba06a251ddde616645c
HeartbrokenRobin <123852082+xhu703981@users.noreply.github.com>
2026-04-11 13:55:58 -0400

    Add frontend UI and CORS middleware

--- scripts/api.py
+++ scripts/api.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
 from scripts.query_wiki import get_answer
+from fastapi.middleware.cors import CORSMiddleware
 
 class QueryRequest(BaseModel):
     question:str
 
 app=FastAPI()
+
+app.add_middleware(
+      CORSMiddleware,
+      allow_origins=["*"],
+      allow_methods=["*"],
+      allow_headers=["*"],
+  )
 
 @app.post("/query")
 def query_endpoint(request:QueryRequest):
